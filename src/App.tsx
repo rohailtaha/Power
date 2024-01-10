32541c36c2bfffa4e331f0471734a9a31e71e8ad
Rohail Taha <taharohail77@gmail.com>
2024-01-10 22:59:26 +0500

    style: make ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
+import { Battery, Grid, Home, Sun } from './icons';
 
 function App() {
-  return <div></div>
+  return (
+    <>
+      <div className='sky'>
+        <Sun />
+      </div>
+      <div className='ground'>
+        <Grid />
+        <div className='home'>
+          <Battery />
+          <Home />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
